ba7b85f2e5fd862763fedabb6879258471e81d35
Vats1727 <vatsalparmar1742002@gmail.com>
2026-09-19 17:05:33 +0530

    Deleted files

--- allysoftsolutions/server/helpers/response.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function jsonResponse($data, $statusCode = 200) {
-    http_response_code($statusCode);
-    header('Content-Type: application/json');
-    echo json_encode($data);
-    exit;
-}
